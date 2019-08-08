5a6f9f44380cddf63184f3e99f918cd500060793
Deran Decker <me@derandecker.com>
2019-08-08 09:47:56 -1000

    Add util methods, Define model class for Product

---
+++ app/src/main/java/com/lambdaschool/android/organization/model/Product.kt
@@ -0,0 +1,16 @@
+package com.lambdaschool.android.organization.model
+
+class Product {
+    val productName: String = ""
+    val productDescription: String = ""
+    val productPrice: Double = 0.00
+    val productTax: Double = 0.00
+    val productDiscount: Double = 0.00
+
+}
+
+//productName - the product's name
+////productDescription - the product's description
+////productPrice - the product's price
+////productTax - the dollar-amount of tax for this item
+////productDiscount - the percent discount for this item
